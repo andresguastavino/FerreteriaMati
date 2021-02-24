f905f8349a3e56681e921584020141a8b038b219
andresguastavino <andresguastavino@gmail.com>
2021-02-24 01:25:01 -0300

    Removed fetchDataProducts

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ export default class App extends Component {
 
         this.fetchData = this.fetchData.bind(this);
         this.fetchDataProducts = this.fetchDataProducts.bind(this);
-        this.fetchDataPicture = this.fetchDataPicture.bind(this);
         this.getHeaders = this.getHeaders.bind(this);
     }
 
@@ -24,74 +23,6 @@ export default class App extends Component {
 
     fetchData() {
         this.fetchDataProducts();
-/*
-        fetch('https://api.mercadolibre.com/sites/MLA/search?nickname=FERRETERIA-TT', headers)
-            .then(response => response.json())
-            .then(data => {
-                let products = [];
-                
-                for(let productData of data.results) {
-                    let product = {
-                        "id": productData.id,
-                        "title": productData.title,
-                        "price": productData.price,
-                        "permalink": productData.permalink,
-                        "image": ''
-                    }
-                }
-            })
-            .catch(error => console.log(error));
-        console.log('here 1');
-        for(let productData of data.results) {
-            let product = {
-                "id": productData.id,
-                "title": productData.title,
-                "price": productData.price,
-                "permalink": productData.permalink,
-                "image": ''
-            }
-
-            let pictureData = await fetch('https://api.mercadolibre.com/pictures/' + productData.thumbnail_id, headers)
-                .then(response => response.json())
-                .catch(error => console.log(error));
-            console.log(pictureData);
-            product.image = pictureData.variations[0].url;
-
-            products.push(product);
-        }
-        console.log('here 2');
-        let productsTotal = data.paging.total;
-        if(productsTotal > 50) {
-            for(let i = 1; i < productsTotal/50 + 1; i++) {
-                data = await fetch('https://api.mercadolibre.com/sites/MLA/search?nickname=FERRETERIA-TT&offset=' + i * 50, headers)
-                    .then(response => response.json())
-                    .catch(error => console.log(error));
-
-                for(let dataProduct of data.results) {
-                    let product = {
-                        "id": dataProduct.id,
-                        "title": dataProduct.title,
-                        "price": dataProduct.price,
-                        "permalink": dataProduct.permalink,
-                        "image": ''
-                    }
-
-                    let dataPicture = await fetch('https://api.mercadolibre.com/pictures/' + dataProduct.thumbnail_id, headers)
-                        .then(response => response.json())
-                        .catch(error => console.log(error));
-                    product.image = dataPicture.variations[0].url;
-
-                    products.push(product);
-                }
-            }
-        }
-        console.log('here 3');
-        this.setState((state) => {
-            return {
-                products: products,
-                done: true
-            }
-        });*/
     }
 
     fetchDataProducts = async() => {
@@ -144,16 +75,6 @@ export default class App extends Component {
         
     }
 
-    fetchDataPicture(product) {
-        const headers = this.getHeaders();
-
-        let image;
-
-
-            
-        return image;
-    }
-
     getHeaders() {
         let origin1 = "https://ferreteria-tt-demo.herokuapp.com";
         let origin2 = "http://localhost:3000";
@@ -163,7 +84,7 @@ export default class App extends Component {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
                 "Authorization": "Bearer APP_USR-2549013216826875-022316-29e292b690343e2d9dda605f35199cad-163399707",
-                "Access-Control-Allow-Origin": origin2
+                "Access-Control-Allow-Origin": origin1
             }
         }
 
